904d932eafe754c35e10ade6ac0c5cc496050814
Nivetha <72033397+Nivi6524@users.noreply.github.com>
2026-02-04 23:22:16 +0530

    Add files via upload

---
+++ run_job.bat
@@ -0,0 +1,13 @@
+@echo off
+echo -------------------------------
+echo Batch file execution started
+echo Input parameter received: %1
+echo Current directory:
+cd
+echo -------------------------------
+
+echo Listing repository files:
+dir
+
+echo -------------------------------
+echo Batch file execution completed
